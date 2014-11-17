2e5a639bf98d49f68d812508a5bbe54a53dfe03d
Srdjan Grubor <sgnn7@sgnn7.org>
2014-11-17 15:24:21 -0600

    Created a simplified launcher for the key_server
    This is what the launcher should look like with the implementation to
    follow. At some point, this fine should be converted to use argparse
    instead of direct argument parsing.

---
+++ key_server.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+#
+#    Copyright 2014 Tobias Mueller <muelli@cryptobitch.de>
+#    Copyright 2014 Andrei Macavei <andrei.macavei89@gmail.com>
+#    Copyright 2014 Srdjan Grubor <sgnn7@sgnn7.org>
+#
+#    This file is part of GNOME Keysign.
+#
+#    GNOME Keysign is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    GNOME Keysign is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with GNOME Keysign.  If not, see <http://www.gnu.org/licenses/>.
+
+import sys
+import keysign.key_server
+
+if __name__ == '__main__':
+    key_filename = "Example data"
+    if len(sys.argv) >= 2:
+        key_filename = sys.argv[1]
+
+    timeout = 5
+    if len(sys.argv) >= 3:
+        timeout = int(sys.argv[2])
+
+    KeyServer(key_filename, timeout).run()
