332bb07c17328c2201b59eb1b0bc52734c09dade
Srdjan Grubor <sgnn7@sgnn7.org>
2015-04-30 22:54:04 -0700

    Formatted the launcher code layout
    It was pretty gnarly so it's a bit cleaner now.

--- gnome-keysign.py
+++ gnome-keysign.py
@@ -1,17 +1,25 @@
 #!/usr/bin/env python
 
-import logging, sys, signal
-logging.basicConfig(stream=sys.stderr, level=logging.DEBUG, format='%(name)s (%(levelname)s): %(message)s')
+import logging
+import sys
+import signal
 
 from gi.repository import GLib
 
 from keysign.MainWindow import MainWindow
 
+logging.basicConfig(stream=sys.stderr,
+                    level=logging.DEBUG,
+                    format='%(name)s (%(levelname)s): %(message)s')
+
 def main():
     app = MainWindow()
 
     try:
-        GLib.unix_signal_add_full(GLib.PRIORITY_HIGH, signal.SIGINT, lambda *args : app.quit(), None)
+        GLib.unix_signal_add_full(GLib.PRIORITY_HIGH,
+                                  signal.SIGINT,
+                                  lambda *args : app.quit(),
+                                  None)
     except AttributeError:
         pass
 
@@ -19,4 +27,3 @@ def main():
     return exit_status
 
 sys.exit(main())
-
