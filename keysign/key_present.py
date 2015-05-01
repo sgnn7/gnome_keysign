14769ca69865731521e4029daf554629ca91fe66
Srdjan Grubor <sgnn7@sgnn7.org>
2015-05-01 13:43:30 -0700

    Minor formatting cleanup of key_present
    Removed out empty lines, reordered imports, applied a bit of
    standardization to hanging lines.

--- keysign/key_present.py
+++ keysign/key_present.py
@@ -17,39 +17,34 @@
 #    You should have received a copy of the GNU General Public License
 #    along with GNOME Keysign.  If not, see <http://www.gnu.org/licenses/>.
 
-from datetime import datetime
 import signal
 import sys
 import argparse
 import logging
 
+from datetime import datetime
 from gi.repository import Gtk, GLib
 from gi.repository import GObject
 
 from monkeysign.gpg import Keyring
 
-# These are relative imports
 from __init__ import __version__
-
-# FIXME: This should move to this file here
 from SignPages import KeyPresentPage
 
 log = logging.getLogger()
 
-
-
 class KeyPresent(Gtk.Application):
     """A demo application showing how to display sufficient details
     about a key such that it can be sent securely.
-    
+
     Note that the main purpose is to enable secure transfer, not
     reviewing key details.  As such, the implementation might change
     a lot, depending on the method of secure transfer.
     """
     def __init__(self, *args, **kwargs):
         #super(Keys, self).__init__(*args, **kwargs)
-        Gtk.Application.__init__(
-            self, application_id="org.gnome.keysign.keypresent")
+        Gtk.Application.__init__(self,
+                                 application_id="org.gnome.keysign.keypresent")
         self.connect("activate", self.on_activate)
         self.connect("startup", self.on_startup)
 
@@ -57,17 +52,14 @@ class KeyPresent(Gtk.Application):
 
         self.key_present_page = None
 
-
     def on_quit(self, app, param=None):
         self.quit()
-
 
     def on_startup(self, app):
         self.log.info("Startup")
         self.window = Gtk.ApplicationWindow(application=app)
         self.window.set_title ("Keysign - Key")
         self.window.add(self.key_present_page)
-
 
     def on_activate(self, app):
         self.log.info("Activate!")
@@ -78,15 +70,13 @@ class KeyPresent(Gtk.Application):
         # we raise the existing window.
         self.window.present()
 
-
     def run(self, args):
         log.debug("running: %s", args)
-        fpr = args
 
+        fpr = args
         self.key_present_page = KeyPresentPage(fpr=fpr)
 
         super(KeyPresent, self).run()
-
 
 def parse_command_line(argv):
     """Parse command line argument. See -h option
@@ -97,10 +87,13 @@ def parse_command_line(argv):
     parser = argparse.ArgumentParser(description='Auxiliary helper program '+
                                                  'to present a key',
                                      formatter_class=formatter_class)
-    parser.add_argument("--version", action="version",
+    parser.add_argument("--version",
+                        action="version",
                         version="%(prog)s {}".format(__version__))
-    parser.add_argument("-v", "--verbose", dest="verbose_count",
-                        action="count", default=0,
+    parser.add_argument("-v", "--verbose",
+                        dest="verbose_count",
+                        action="count",
+                        default=0,
                         help="increases log verbosity for each occurence.")
     #parser.add_argument("-g", "--gpg",
     #                    action="store_true", default=False,
@@ -110,7 +103,8 @@ def parse_command_line(argv):
     #                    help="redirect output to a file")
     #parser.add_argument('file', help='File to read keydata from ' +
     #                                 '(or KeyID if --gpg is given)')
-    parser.add_argument('fpr', help='The fingerprint of the key to transfer')
+    parser.add_argument('fpr',
+                        help='The fingerprint of the key to transfer')
     ## nargs='+', # argparse.REMAINDER,
     #parser.add_argument('input', metavar="input",
     ## nargs='+', # argparse.REMAINDER,
@@ -118,8 +112,8 @@ def parse_command_line(argv):
     arguments = parser.parse_args(argv[1:])
     # Sets log level to WARN going more verbose for each new -v.
     log.setLevel(max(3 - arguments.verbose_count, 0) * 10)
-    return arguments
 
+    return arguments
 
 def main(args=sys.argv):
     """This is an example program of how to use the PresentKey widget"""
@@ -127,7 +121,7 @@ def main(args=sys.argv):
                         format='%(name)s (%(levelname)s): %(message)s')
     try:
         arguments = parse_command_line(args)
-        
+
         #if arguments.gpg:
         #    keyid = arguments.file
         #    keyring = Keyring()
@@ -146,12 +140,10 @@ def main(args=sys.argv):
             GLib.unix_signal_add_full(GLib.PRIORITY_HIGH, signal.SIGINT, lambda *args : app.quit(), None)
         except AttributeError:
             pass
-    
-        exit_status = app.run(fpr)
-    
-        return exit_status
 
-        
+        exit_status = app.run(fpr)
+
+        return exit_status
     finally:
         logging.shutdown()
 
